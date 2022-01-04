333a0da6275853c50fee81f12899c2c342fd0dab
Matthew Thorning <matthew@ometria.com>
2022-01-04 20:58:40 +0000

    focusable, draggable windows

---
+++ src/atoms/WindowHeader.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import MinimiseIcon from 'svelte-icons/io/IoMdRemove.svelte';
+  import MaximiseIcon from 'svelte-icons/io/IoIosSquareOutline.svelte';
+  import CloseIcon from 'svelte-icons/io/IoIosClose.svelte';
+  export let hasFocus = true;
+  export let title: string = 'New Window';
+</script>
+
+<div class:focused={hasFocus}
+    on:touchstart
+    on:touchend
+    on:touchmove
+    on:mousedown
+    on:mouseup
+    on:mousemove
+  >
+  <h1>{title}</h1>
+  <nav>
+    <button> <MinimiseIcon strokeSize={4} /> </button>
+    <button> <MaximiseIcon /> </button>
+    <button class="close"> <CloseIcon /> </button>
+  </nav>
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: space-between;
+    height: 29px;
+  }
+  h1 {
+    font-size: 12px;
+    font-weight: 400;
+    padding-left: 8px;
+    margin: 0;
+    display: flex;
+    align-self: center;
+  }
+  div,
+  button {
+    background: var(--lightest-black);
+    color: var(--grey);
+  }
+  nav {
+    display: flex;
+    justify-self: flex-end;
+  }
+  button {
+    border: none;
+  }
+  .focused,
+  .focused button {
+    background: var(--darkest-black);;
+    color: var(--lightest-white);
+  }
+  button:hover,
+  .focused button:hover {
+    background: var(--light-black);
+    color: var(--lightest-white);
+  }
+  button.close:hover,
+  .focused button.close:hover {
+    background: var(--red);
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,62 @@
+<script context="module" lang="ts">
+  import type { Writable } from 'svelte/store';
+  import { writable } from 'svelte/store';
+  import Window from '../molecules/window.svelte';
+
+  export interface ScreenMeta {
+    height?: number;
+    width?: number;
+    focused?: EventTarget;
+  }
+</script>
+
+<script lang="ts">
+  const screenMeta: Writable<ScreenMeta> = writable();
+
+  function resizeObserver(el: HTMLElement) {
+    const resizeObserver = new ResizeObserver(([entry]) => {
+      const { height, width } = entry.contentRect;
+      screenMeta.set({ height, width });
+    });
+
+    resizeObserver.observe(el);
+
+    return {
+      destroy() {
+        resizeObserver.unobserve(el);
+      }
+    };
+  }
+function setFocus(e) {
+  screenMeta.update((vals) => ({ ...vals, focused: e.target }))
+}
+</script>
+
+<div use:resizeObserver>
+  <Window screenMeta={$screenMeta} on:mousedown={setFocus} />
+  <Window screenMeta={$screenMeta} on:mousedown={setFocus} />
+</div>
+
+<style>
+  :root {
+    --darkest-black: rgb(0, 0, 0);
+    --medium-black: rgb(32, 32, 32);
+    --light-black: rgb(43, 43, 43);
+    --lightest-black: rgb(26, 26, 26);
+    --grey: rgb(127, 127, 127);
+    --dark-white: #222121;
+    --lightest-white: rgb(255, 255, 255);
+
+    --red: rgb(232, 17, 35);
+  }
+  :global(body) {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+      'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  div {
+    position: relative;
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
